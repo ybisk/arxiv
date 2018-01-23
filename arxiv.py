53db808634ef914192690d508c6188d30fb44741
ybisk <ybisk@yo-tech.com>
2018-01-22 23:54:49 -0800

    assume notes are the conference

--- arxiv.py
+++ arxiv.py
@@ -5,13 +5,14 @@ import xml.etree.ElementTree as ET
 
 out = open("download.bib",'w')
 base = "http://export.arxiv.org/api/query?id_list="
-identifiers = sys.argv[1:] #"1801.02270v1"
+identifiers = sys.argv[1:]
 for identifier in identifiers:
   url = base + identifier
   tree = ET.parse(urllib.request.urlopen(url))
   root = tree.getroot()
 
   title = ""
+  conference = None
   authors = []
   url = "https://arxiv.org/abs/" + identifier
   for child in root:
@@ -25,10 +26,16 @@ for identifier in identifiers:
         date = grand.text.split("-")
         year = date[0]
         month = date[1]
+      elif "}comment" in grand.tag:
+        conference = grand.text
 
   subprocess.call("wget --user-agent=Lynx https://arxiv.org/pdf/" + identifier + ".pdf", shell=True)
   citekey = authors[0].split()[-1] + ":" + year
-  out.write("@unpublished{" + citekey + ",\n")
+  if conference is None:
+    out.write("@unpublished{" + citekey + ",\n")
+  else:
+    out.write("@inproceedings{" + citekey + ",\n")
+    out.write("\tBooktitle = {" + conference + "},\n")
   out.write("\tAuthor = {" + " and ".join(authors) + " },\n")
   out.write("\tTitle = {{" + title + "}},\n")
   out.write("\tUrl = {" + url + "},\n")
